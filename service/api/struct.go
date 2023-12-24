a26744ffb0457066484d7fd1632b08a157aeb989
Michele <fontana.1905755@studenti.uniroma1.it>
2023-12-24 12:47:14 +0100

    minor changes

--- service/api/struct.go
+++ service/api/struct.go
@@ -1,16 +1,20 @@
 package api
 
+import (
+	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/database"
+)
+
 // struct for the Profile
 type Profile struct {
 	Id             uint64 `json:"userId"`
-	requesterId    uint64 `json:"requesterId"`
+	RequesterId    uint64 `json:"requesterId"`
 	Username       string `json:"username"`
 	FollowerCount  int    `json:"followerCount"`
 	FollowingCount int    `json:"followingCount"`
 	PostCount      int    `json:"postCount"`
 	IsFollowing    bool   `json:"isFollowing"`
-	checkBanned    bool   `json:"checkBanned"`
-	banned         bool   `json:"banned"`
+	CheckBanned    bool   `json:"checkBanned"`
+	Banned         bool   `json:"banned"`
 }
 
 // struct for the User
@@ -19,6 +23,7 @@ type User struct {
 	Username string `json:"username"`
 }
 
+// struct for the PhotoStream
 type PhotoStream struct {
 	Id           uint64 `json:"id"`
 	UserId       uint64 `json:"userId"`
@@ -28,12 +33,14 @@ type PhotoStream struct {
 	CommentCount int    `json:"commentCount"`
 }
 
+// struct for the Follow
 type Follow struct {
 	Id       uint64 `json:"id"`
 	UserId   uint64 `json:"userId"`
 	FollowId uint64 `json:"followId"`
 }
 
+// struct for the Like
 type Like struct {
 	LikeId     uint64 `json:"id"`
 	UserId     uint64 `json:"userId"`
@@ -41,12 +48,14 @@ type Like struct {
 	PhotoOwner string `json:"likeType"`
 }
 
+// struct for the Ban
 type Ban struct {
 	RequestingBanId uint64 `json:"id"`
 	UserId          uint64 `json:"userId"`
 	BanId           uint64 `json:"banId"`
 }
 
+// struct for the Photo
 type Photo struct {
 	Id           uint64 `json:"id"`
 	UserId       uint64 `json:"userId"`
@@ -56,10 +65,17 @@ type Photo struct {
 	CommentCount int    `json:"comment"`
 }
 
+// struct for the Comment
+
 type Comment struct {
 	Id         uint64 `json:"id"`
 	UserId     uint64 `json:"userId"`
 	PhotoId    uint64 `json:"photoId"`
 	PhotoOwner string `json:"photoOwner"`
 	Comment    string `json:"comment"`
+}
+
+func (u *User) FromDatabase(user database.User) {
+	u.Id = user.Id
+	u.Username = user.Username
 }
