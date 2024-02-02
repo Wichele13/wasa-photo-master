8c1dd52a4b7d2320cec2a4c1698b4fb030464022
Michele <fontana.1905755@studenti.uniroma1.it>
2024-02-02 17:25:43 +0100

    added structs

--- service/database/database.go
+++ service/database/database.go
@@ -36,6 +36,110 @@ import (
 	"fmt"
 )
 
+// Errors
+var ErrUserDoesNotExist = errors.New("User does not exist")
+var ErrPhotoDoesNotExist = errors.New("Photo does not exist")
+var ErrBanDoesNotExist = errors.New("Ban does not exist")
+var ErrFollowDoesNotExist = errors.New("Follow does not exist")
+var ErrCommentDoesNotExist = errors.New("Comment does not exist")
+var ErrLikeDoesNotExist = errors.New("Like does not exist")
+
+// User is the struct for the user
+type User struct {
+	Id       uint64 `json:"id"`
+	Username string `json:"username"`
+}
+
+// Stream is the struct for the photo stream
+type Stream struct {
+	Identifier uint64        `json:"identifier"`
+	Photos     []PhotoStream `json:"photoStream"`
+}
+
+// PhotoStream is the struct for the photo stream
+type PhotoStream struct {
+	Id           uint64 `json:"id"`
+	UserId       uint64 `json:"userId"`
+	Username     string `json:"username"`
+	File         []byte `json:"file"`
+	Date         string `json:"date"`
+	LikeCount    int    `json:"likeCount"`
+	CommentCount int    `json:"commentCount"`
+	LikeStatus   bool   `json:"likeStatus"`
+}
+
+// Followers is the struct for the followers
+type Followers struct {
+	Id        uint64   `json:"identifier"`
+	Followers []Follow `json:"Followers"`
+}
+
+// Follow is the struct for the follow
+type Follow struct {
+	FollowId   uint64 `json:"followId"`
+	FollowedId uint64 `json:"followedId"`
+	UserId     uint64 `json:"userId"`
+}
+
+// Bans is the struct for the bans
+type Bans struct {
+	Identifier uint64 `json:"identifier"`
+	Username   string `json:"username"`
+	Bans       []Ban  `json:"bans"`
+}
+
+// Ban is the struct for the ban
+type Ban struct {
+	BanId    uint64 `json:"banId"`
+	BannedId uint64 `json:"bannedId"`
+	UserId   uint64 `json:"userId"`
+}
+
+// Photos is the struct for the photos
+type Photos struct {
+	RequestUser uint64  `json:"requestUser"`
+	Identifier  uint64  `json:"identifier"`
+	Photos      []Photo `json:"photos"`
+}
+
+// Photo is the struct for the photo
+type Photo struct {
+	Id            uint64 `json:"id"`
+	UserId        uint64 `json:"userId"`
+	File          []byte `json:"file"`
+	Date          string `json:"date"`
+	LikesCount    int    `json:"likesCount"`
+	CommentsCount int    `json:"commentsCount"`
+	LikeStatus    bool   `json:"likeStatus"`
+}
+
+// Like is the struct for the like
+type Like struct {
+	LikeId          uint64 `json:"likeId"`
+	UserIdentifier  uint64 `json:"identifier"`
+	PhotoIdentifier uint64 `json:"photoIdentifier"`
+	PhotoOwner      uint64 `json:"photoOwner"`
+}
+
+// Comments is the struct for the comments
+type Comments struct {
+	RequestIdentifier uint64    `json:"requestIdentifier"`
+	PhotoIdentifier   uint64    `json:"photoIdentifier"`
+	PhotoOwner        uint64    `json:"identifier"`
+	Comments          []Comment `json:"comments"`
+}
+
+// Comment is the struct for the comment
+type Comment struct {
+	Id            uint64 `json:"id"`
+	UserId        uint64 `json:"userId"`
+	PhotoId       uint64 `json:"photoId"`
+	PhotoOwner    uint64 `json:"photoOwner"`
+	OwnerUsername string `json:"ownerUsername"`
+	Username      string `json:"username"`
+	Content       string `json:"content"`
+}
+
 // AppDatabase is the high level interface for the DB
 type AppDatabase interface {
 	GetName() (string, error)
