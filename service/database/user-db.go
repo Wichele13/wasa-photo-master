9aa5f97c50591d5b914628e7f6e7d2d7caaeb9e6
Michele <fontana.1905755@studenti.uniroma1.it>
2024-02-03 12:04:51 +0100

    continued createUser func

--- service/database/user-db.go
+++ service/database/user-db.go
@@ -1,6 +1,25 @@
 package database
 
+import (
+	"database/sql"
+)
+
 func (db *appdbimpl) CreateUser(u User) (User, error) {
 	res, err := db.c.Exec("INSERT INTO users (username) VALUES (?, ?, ?)", u.Username)
+	if err != nil {
+		var user User
+		if err := db.c.QueryRow(`SELECT id, username FROM users WHERE username = ?`, u.Username).Scan(&user.Id, &user.Username); err != nil {
+			if err == sql.ErrNoRows {
+				return user, ErrUserDoesNotExist
+			}
+		}
+		return user, nil
+
+	}
+	lastInsertId, err := res.LastInsertId()
+
+	if err != nil {
+		return u, err
+	}
 
 }
