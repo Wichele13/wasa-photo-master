6f5ea4a82f05a55b4c1b9cb6d7552a4548251a1c
Michele <fontana.1905755@studenti.uniroma1.it>
2024-04-17 15:19:06 +0200

    changes

--- service/api/struct.go
+++ service/api/struct.go
@@ -1,10 +1,10 @@
 package api
 
-/*
+
 import (
 	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/database"
 )
-*/
+
 
 // struct for the Profile
 type Profile struct {
@@ -77,9 +77,15 @@ type Comment struct {
 	Comment    string `json:"comment"`
 }
 
-/*
+
 func (u *User) FromDatabase(user database.User) {
 	u.Id = user.Id
 	u.Username = user.Username
 }
-*/
+
+func (u *User) ToDatabase() database.User {
+	return database.User{
+		Id:       u.Id,
+		Username: u.Username,
+	}
+}
