b6913a7c8dacbfa329302f44e2bab5f401e9e61c
Michele <fontana.1905755@studenti.uniroma1.it>
2024-04-20 15:29:16 +0200

    added followActions

--- service/api/followActions.go
+++ service/api/followActions.go
@@ -1,1 +1,97 @@
 package api
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strconv"
+
+	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/api/reqcontext"
+	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/database"
+	"github.com/julienschmidt/httprouter"
+)
+
+//FUNZIONE CHE ESEGUE IL FOLLOW
+func (rt *_router) followUser(w http.ResponseWriter, r *http.Request, ps httprouter.Params, ctx reqcontext.RequestContext) {
+	var follow Follow
+	var user User
+	username := ps.ByName("username")
+	dbuser, err := rt.db.GetUserId(username)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	user.FromDatabase(dbuser)
+	id, err := strconv.ParseUint(ps.ByName("followid"), 10, 64)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	follow.FollowId = id
+	follow.FollowedId = user.Id
+	token := getToken(r.Header.Get("Authorization"))
+	follow.UserId = token
+	dbfollow, err := rt.db.AddFollow(follow.FollowToDatabase())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	follow.FollowFromDatabase(dbfollow)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	_ = json.NewEncoder(w).Encode(follow)
+}
+
+//FUNZIONE CHE ESEGUE L'UNFOLLOW
+func (rt *_router) unfollowUser(w http.ResponseWriter, r *http.Request, ps httprouter.Params, ctx reqcontext.RequestContext) {
+	var follow Follow
+	var user User
+	username := ps.ByName("username")
+	dbuser, err := rt.db.GetUserId(username)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	user.FromDatabase(dbuser)
+	id, err := strconv.ParseUint(ps.ByName("followid"), 10, 64)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	follow.FollowId = id
+	follow.FollowedId = user.Id
+	token := getToken(r.Header.Get("Authorization"))
+	follow.UserId = token
+	err = rt.db.RemoveFollow(follow.FollowId, follow.UserId, follow.FollowedId)
+	if errors.Is(err, database.ErrBanDoesNotExist) {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	} else if err != nil {
+		ctx.Logger.WithError(err).WithField("id", id).Error("Impossibile rimuovere il follow")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
+//FUNZIONE CHE RESTITUISCE LA LISTA DEI FOLLOWERS
+func (rt *_router) getFollowers(w http.ResponseWriter, r *http.Request, ps httprouter.Params, ctx reqcontext.RequestContext) {
+	var user User
+	var follow Follow
+	token := getToken(r.Header.Get("Authorization"))
+	user.Username = ps.ByName("username")
+	dbuser, err := rt.db.CheckUserByUsername(user.ToDatabase())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	user.FromDatabase(dbuser)
+	dbfollow, err := rt.db.GetFollowers(user.ToDatabase(), token)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	follow.FollowFromDatabase(dbfollow)
+	w.Header().Set("Content-Type", "application/json")
+	_ = json.NewEncoder(w).Encode(follow)
+}
