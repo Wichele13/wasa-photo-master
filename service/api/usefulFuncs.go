528ab3a29f7be9d6481033a9ac1495cf9ec83a54
Michele <fontana.1905755@studenti.uniroma1.it>
2024-04-22 16:25:37 +0200

    little changes

--- service/api/usefulFuncs.go
+++ service/api/usefulFuncs.go
@@ -1,15 +1,23 @@
 package api
 
 import (
-	"github.com/julienschmidt/httprouter"
 	"net/http"
+	"regexp"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
 )
 
+func getToken(message string) uint64 {
+	re := regexp.MustCompile(`[-]?\d[\d,]*[\.]?[\d{2}]*`)
+	stringToken := re.FindAllString(message, -1)
+	token, _ := strconv.Atoi(stringToken[0])
+	return uint64(token)
+}
 
 func (rt *_router) Close() error {
 	return nil
 }
-
 
 func (rt *_router) liveness(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 
