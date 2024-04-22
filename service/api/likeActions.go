a5973e6ebf1a25908539ebf1503f43cdb49855ff
Michele <fontana.1905755@studenti.uniroma1.it>
2024-04-22 12:04:36 +0200

    starting like actions

---
+++ service/api/likeActions.go
@@ -0,0 +1,12 @@
+package api
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strconv"
+
+	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/api/reqcontext"
+	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/database"
+	"github.com/julienschmidt/httprouter"
+)
