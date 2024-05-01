060c27798e1e73eb16f8f31c8ee9d778922b79db
Michele <fontana.1905755@studenti.uniroma1.it>
2024-05-01 09:42:52 +0200

    starting vue

--- webui/src/views/HomeView.vue
+++ webui/src/views/HomeView.vue
@@ -28,8 +28,7 @@ export default {
 
 <template>
 	<div>
-		<div
-			class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+		<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
 			<h1 class="h2">Home page</h1>
 			<div class="btn-toolbar mb-2 mb-md-0">
 				<div class="btn-group me-2">
