cb8d6f432a89d9bb30e6a6fc03d3d1d789733483
Michele <fontana.1905755@studenti.uniroma1.it>
2024-05-04 08:51:38 +0200

    corrected paths

--- service/api/api-handler.go
+++ service/api/api-handler.go
@@ -9,34 +9,34 @@ func (rt *_router) Handler() http.Handler {
 
 	// userActions
 	rt.router.POST("/session", rt.wrap(rt.doLogin))
-	rt.router.PUT("/user/:username/setusername", rt.wrap(rt.setMyUserName))
+	rt.router.PUT("/user/:username/setUsername", rt.wrap(rt.setMyUserName))
 	rt.router.GET("/user/:username/stream", rt.wrap(rt.getMyStream))
 	rt.router.GET("/users/:username/profile", rt.wrap(rt.getUserProfile))
 
 	// PhotoActions
-	rt.router.PUT("/users/:username/photo/:photoid", rt.wrap(rt.uploadPhoto))
-	rt.router.DELETE("/users/:username/photo/:photoid", rt.wrap(rt.deletePhoto))
+	rt.router.PUT("/users/:username/photo/:photoId", rt.wrap(rt.uploadPhoto))
+	rt.router.DELETE("/users/:username/photo/:photoId", rt.wrap(rt.deletePhoto))
 	rt.router.GET("/users/:username/photo", rt.wrap(rt.getUserPhotos))
 
 	// BanActions
-	rt.router.PUT("/users/:username/ban/:banid", rt.wrap(rt.banUser))
-	rt.router.DELETE("/users/:username/ban/:banid", rt.wrap(rt.unbanUser))
+	rt.router.PUT("/users/:username/ban/:banId", rt.wrap(rt.banUser))
+	rt.router.DELETE("/users/:username/ban/:banId", rt.wrap(rt.unbanUser))
 	rt.router.GET("/users/:username/ban", rt.wrap(rt.getBans))
 
 	// FollowActions
-	rt.router.PUT("/users/:username/follow/:followid", rt.wrap(rt.followUser))
-	rt.router.DELETE("/users/:username/follow/:followid", rt.wrap(rt.unfollowUser))
+	rt.router.PUT("/users/:username/follow/:followId", rt.wrap(rt.followUser))
+	rt.router.DELETE("/users/:username/follow/:followId", rt.wrap(rt.unfollowUser))
 	rt.router.GET("/users/:username/follow", rt.wrap(rt.getFollowers))
 
 	// LikeActions
-	rt.router.PUT("/users/:username/photo/:photoid/like/:likeid", rt.wrap(rt.likePhoto))
-	rt.router.DELETE("/users/:username/photo/:photoid/like/:likeid", rt.wrap(rt.unlikePhoto))
-	rt.router.GET("/users/:username/photo/:photoid/like", rt.wrap(rt.getLikes))
+	rt.router.PUT("/users/:username/photo/:photoId/likes/:likeId", rt.wrap(rt.likePhoto))
+	rt.router.DELETE("/users/:username/photo/:photoId/likes/:likeId", rt.wrap(rt.unlikePhoto))
+	rt.router.GET("/users/:username/photo/:photoId/likeS", rt.wrap(rt.getLikes))
 
 	// CommentActions
-	rt.router.PUT("/users/:username/photo/:photoid/comment/:commentid", rt.wrap(rt.commentPhoto))
-	rt.router.DELETE("/users/:username/photo/:photoid/comment/:commentid", rt.wrap(rt.uncommentPhoto))
-	rt.router.GET("/users/:username/photo/:photoid/comment", rt.wrap(rt.getComments))
+	rt.router.PUT("/users/:username/photos/:photoId/comment/:commentId", rt.wrap(rt.commentPhoto))
+	rt.router.DELETE("/users/:username/photos/:photoId/comment/:commentId", rt.wrap(rt.uncommentPhoto))
+	rt.router.GET("/users/:username/photos/:photoId/comment", rt.wrap(rt.getComments))
 
 	// Liveness
 	rt.router.GET("/liveness", rt.liveness)
